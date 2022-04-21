766b8759968e77f93d6ed8aa34fc90b2cba63593
Ash Mackay <ash.mackay85@gmail.com>
2022-04-21 22:45:11 +0100

    Validation with Decorators - Bug Fix

--- src/app.ts
+++ src/app.ts
@@ -140,14 +140,20 @@ const registeredValidators: ValidatorConfig = {};
 function required(target: any, propName: string) {
   registeredValidators[target.constructor.name] = {
     ...registeredValidators[target.constructor.name],
-    [propName]: ["required"],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "required",
+    ],
   };
 }
 
 function PositiveNumber(target: any, propName: string) {
   registeredValidators[target.constructor.name] = {
     ...registeredValidators[target.constructor.name],
-    [propName]: ["positive"],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "positive",
+    ],
   };
 }
 
